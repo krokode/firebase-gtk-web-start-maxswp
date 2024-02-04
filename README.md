a58f5c6e2edd85fd574e921932157421ea05eb76
krokode <95712836+krokode@users.noreply.github.com>
2024-02-04 18:27:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# firebase-gtk-web-start-maxswp
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/krokode/firebase-gtk-web-start-maxswp)
---
+++ index.js
@@ -0,0 +1,132 @@
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required
+import { initializeApp } from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import { getAuth,
+   EmailAuthProvider,
+   signOut,
+   onAuthStateChanged
+  } from 'firebase/auth';
+
+import {
+   getFirestore,
+   addDoc,
+   collection,
+   query,
+   orderBy,
+   onSnapshot
+  } from 'firebase/firestore';
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+let rsvpListener = null;
+let guestbookListener = null;
+
+let db, auth;
+
+async function main() {
+  // Add Firebase project configuration object here
+  const firebaseConfig = {
+    apiKey: "AIzaSyD3TDZAGMd7IlArnGpHApfFHflqQzfY81w",
+    authDomain: "fir-web-codelab-65364.firebaseapp.com",
+    projectId: "fir-web-codelab-65364",
+    storageBucket: "fir-web-codelab-65364.appspot.com",
+    messagingSenderId: "686471591694",
+    appId: "1:686471591694:web:2467a9c3bfbdbfa342495f"
+  };
+
+  // Initializa Firebase 
+  initializeApp(firebaseConfig);
+  auth = getAuth();
+  db = getFirestore();
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      },
+    },
+  };
+
+  // const ui = new firebaseui.auth.AuthUI(auth);
+  // Initialize the FirebaseUI widget using Firebase
+  const ui = new firebaseui.auth.AuthUI(auth);
+
+  startRsvpButton.addEventListener('click', () => {
+    if (auth.currentUser) {
+      // User is signed in; allows user to sign out
+      signOut(auth);
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
+  });
+
+  // Listen to the current Auth state
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      startRsvpButton.textContent = 'LOGOUT';
+      // Show guestbook to logged-in users
+      guestbookContainer.style.display = 'block';
+    } else {
+      startRsvpButton.textContent = 'LOGIN';
+      // Hide guestbook for non-logged-in users
+      guestbookContainer.style.display = 'none';
+    }
+  });
+
+  // Listen to the form submission
+ form.addEventListener('submit', async e => {
+  // Prevent the default form redirect
+  e.preventDefault();
+  // Write a new message to the database collection "guestbook"
+  addDoc(collection(db, 'guestbook'), {
+    text: input.value,
+    timestamp: Date.now(),
+    name: auth.currentUser.displayName,
+    userId: auth.currentUser.uid
+  });
+  // clear message input field
+  input.value = '';
+  // Return false to avoid redirect
+  return false;
+  });
+
+  // Create query for messages
+  const q = query(collection(db, 'guestbook'), orderBy('timestamp', 'desc'));
+  onSnapshot(q, snaps => {
+    // Reset page
+    guestbook.innerHTML = '';
+    // Loop through documents in database
+    snaps.forEach(doc => {
+      // Create an HTML entry for each document and add it to the chat
+      const entry = document.createElement('p');
+      entry.textContent = doc.data().name + ': ' + doc.data().text;
+      guestbook.appendChild(entry);
+    });
+  });
+
+}
+
+main();
